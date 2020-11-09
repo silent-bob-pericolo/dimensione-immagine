ebdd2bade49adfb0f28e2e8e3d076df094af86ba
alessandro <m.cherubini52@gmail.com>
2020-11-09 11:09:52 +0100

    Aggiungere i file di progetto.

---
+++ dimensioneimmagine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dimensioneimmagine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il primo valore:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo valore:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int calcolo = ((n1 + n2) + 3) / 1024;
+            Console.WriteLine($"L'immagine pesa {calcolo} KB");
+        }
+    }
+}
